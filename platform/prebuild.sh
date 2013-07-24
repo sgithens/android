d2976cd4eaac87832b075032c5b1e198513b30ca
Javier Hernández <jhernandez@emergya.com>
2013-07-24 17:50:29 +0200

    Fixing build minor issues
     * marking makestubs.sh as exectuable
     * checking for app/assets directory
     * some code cleanup

--- platform/prebuild.sh
+++ platform/prebuild.sh
@@ -13,11 +13,11 @@ fi
 export ANODE_ROOT=$(pwd)/anode
 
 # Fetch node binaries from webinos
+if [ -d 'app/assets' ]; then
+    echo 'assets directory already created!'
+else
+    mkdir -p app/assets
+fi
+
 curl -o app/assets/bridge.node https://raw.github.com/webinos/Webinos-Platform/master/webinos/platform/android/app/assets/bridge.node
 curl -o app/assets/libjninode.so https://raw.github.com/webinos/Webinos-Platform/master/webinos/platform/android/app/assets/libjninode.so
-
-# java -jar ./anode/sdk/java/tools/stubgen.jar --verbose --out ./intents/src --classpath ./app/bin/classes net.gpii.AndroidIntentHandler
-
-# java -jar ./anode/sdk/java/tools/stubgen.jar --verbose --out ./a11yservices/src --classpath ./app/bin/classes net.gpii.AndroidA11ySettings
-
-# java -jar ./anode/sdk/java/tools/stubgen.jar --verbose --out ./nativesettings/src --classpath ./app/bin/classes net.gpii.AndroidFontSettings
